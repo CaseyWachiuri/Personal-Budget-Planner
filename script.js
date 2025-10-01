b5d2c290015ebfb1c680892611291118b2a8c5c8
Casey Waweru <casey.waweru@gmail.com>
2025-10-01 11:23:40 +0300

    Redoing the interface and making it a single-page web application

---
+++ script.js
@@ -0,0 +1,13 @@
+const init = () => {
+  console.log("Hello there");
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+// Function for adding expenses
+function addExpense() {
+}
+
+// Function for adding income
+function addIncome() {
+}
